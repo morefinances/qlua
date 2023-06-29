878c8c82564dc36b9604abbf59c8af3314205ae7
alfacentavra <49415297+morefinances@users.noreply.github.com>
2023-06-29 14:19:09 +0300

    Add files via upload
    Скрипт делает выгрузку всех торгуемых акций на Московской Бирже (иногда встречаются варианты с нулевыми объемами, но с индикативными ценами). 
    Сохраняет результат в файл C:\\files\\tickers.csv.
    Параллельно выводит список с разбивкой по 5 бумаг в строку в терминале.

---
+++ moextickers.lua
@@ -0,0 +1,42 @@
+function main()
+
+	message('[ = = = = = = = = = = start = = = = = = = = = = ]')
+	
+	datetime = os.date("!*t",os.time())
+	message('Определение тикеров торгуемых бумаг на фондовой секции')
+	message(os.date("%d.%m.%Y"))
+	message('Начало работы скрипта '..os.date("%X",os.time()))
+	
+
+	DirectionSaveFile=tostring("C:\\files\\tickers.csv") 
+	my_csv=io.open(DirectionSaveFile,"w") 
+	
+	sec_list = getClassSecurities("TQBR") -- тикеры в одну строчку
+	
+	ind = 1 -- индекс подсчета количество бумаг
+	sprint = "" -- склейка массивов для вывода
+	
+	-- разбивка строки с тикерами
+	for TIKER in string.gmatch(sec_list, "[^,]+") do
+		-- запись в файл
+		my_csv:write(TIKER.."\n")
+		sprint = sprint..tostring(ind).."/ "..TIKER.."  "
+		ind = ind + 1 -- индекс бумаги
+		if ind%5 == 1 then 
+			message(sprint)
+			sprint = ""
+		end
+		sleep(5)
+	end
+	
+	if sprint ~= "" then message(sprint) end 
+	
+	-- -- закрытие файла
+	my_csv:flush()  
+	my_csv:close() 
+
+	message('Завершение работы скрипта '.. os.date("%X",os.time()))
+	message('Общее количество торгуемых бумаг : '..tostring(ind - 1))
+	message("[ = = = = = = = = = = end = = = = = = = = = =  ]")
+	
+end
